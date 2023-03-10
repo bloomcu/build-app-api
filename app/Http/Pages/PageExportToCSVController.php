3941aa64ba93641120ea2790f4d051f2418a1302
Ryan Harmon <ryan@bloomcu.com>
2023-03-09 17:46:23 -0700

    Cleanup and remove dashed from titles

--- app/Http/Pages/PageExportToCSVController.php
+++ app/Http/Pages/PageExportToCSVController.php
@@ -44,7 +44,7 @@ class PageExportToCSVController extends Controller
             fputcsv($file, $columns);
 
             foreach ($pages as $page) {
-                $row['Title'] = str_replace(chr(194), '', $page->formattedTitle);
+                $row['Title'] = str_replace(chr(194), '', $page->title);
                 $row['Parents'] = str_replace(chr(194), '', $page->parents);
                 $row['Url'] = str_replace(chr(194), '', $page->url);
                 $row['Category'] = str_replace(chr(194), '', $page->category ? $page->category->title : 'Uncategorized');
@@ -104,38 +104,4 @@ class PageExportToCSVController extends Controller
         }
         return $output;
     }
-
-    // public function flatten($input, $key)
-    // {
-    //     // $level = 0;
-    //     $output = [];
-    //
-    //     // For each object in the array
-    //     foreach ($input as $object) {
-    //
-    //         // separate its children
-    //         // if (isset($object->$key)) {
-    //         //     $children = $object->$key;
-    //         // } else {
-    //         //     $children = [];
-    //         // }
-    //         // $object->$key = [];
-    //
-    //         // separate its children
-    //         $children = isset($object->$key) ? $object->$key : [];
-    //         $object->$key = [];
-    //
-    //         // and add it to the output array
-    //         $output[] = $object;
-    //
-    //         // Recursively flatten the array of children
-    //         $children = $this->flatten($children, $key);
-    //
-    //         //  and add the result to the output array
-    //         foreach ($children as $child) {
-    //             $output[] = $child;
-    //         }
-    //     }
-    //     return $output;
-    // }
 }
