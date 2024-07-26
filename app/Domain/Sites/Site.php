4f0d13cd604ce190884a037b38ca487038b55865
Bryan <bryan.e.dev@gmail.com>
2024-07-26 14:03:00 -0500

    FIX : Remove duplicate namespace declarations

--- app/Domain/Sites/Site.php
+++ app/Domain/Sites/Site.php
@@ -2,18 +2,16 @@
 
 namespace DDD\Domain\Sites;
 
-use DDD\App\Traits\BelongsToOrganization;
-use DDD\Domain\Base\Sites\Casts\LaunchInfo;
-// Casts
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 // Traits
+use DDD\App\Traits\BelongsToOrganization;
+// Casts
+
+use DDD\Domain\Base\Sites\Casts\LaunchInfo;
+
+// Database
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// Casts
-use DDD\Domain\Sites\Casts\LaunchInfo;
-
-// Traits
-use DDD\App\Traits\BelongsToOrganization;
 
 class Site extends Model
 {
